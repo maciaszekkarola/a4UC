d5ea4f39087cd4e196c2c502bace42dcb543b54d
Karolina <carolha.es@gmail.com>
2017-09-29 12:34:29 +0200

    directive introduction

--- directives-start/directives-start/src/app/app.component.ts
+++ directives-start/directives-start/src/app/app.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   numbers = [1, 2, 3, 4, 5];
+  oddNumbers = [];
   onlyOdd = false;
+
+  ngOnInit() {
+    this.numbers.forEach(element => {
+      if (element % 2 !== 0) {
+        this.oddNumbers.push(element);
+      }
+    });
+  }
+
 }
+
+
+
+
