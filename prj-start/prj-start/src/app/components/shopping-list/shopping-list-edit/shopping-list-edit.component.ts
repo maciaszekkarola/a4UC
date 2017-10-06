94e03ebd837986456c7b210d0364d50f6a172086
Karolina <carolha.es@gmail.com>
2017-10-06 14:57:45 +0200

    project form

--- prj-start/prj-start/src/app/components/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ prj-start/prj-start/src/app/components/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../../../models/ingredient.model';
 import {  ShoppingListService } from '../shopping-list.service';
 
@@ -8,20 +9,16 @@ import {  ShoppingListService } from '../shopping-list.service';
   styleUrls: ['./shopping-list-edit.component.css']
 })
 export class ShoppingListEditComponent implements OnInit {
-  @ViewChild('nameInput') nameInputRef: ElementRef;
-  @ViewChild('amountInput') amountInputRef: ElementRef;
   
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
   }
-  onAdd(nameInput: HTMLInputElement, amountInput: HTMLInputElement) {
-    const ingName = this.nameInputRef.nativeElement.value;
-    const ingAmount = this.amountInputRef.nativeElement.value;
-    const newIngredient = new Ingredient(ingName, ingAmount);
 
+  onAddItem(form: NgForm) {
+    const value = form.value;
+    const newIngredient = new Ingredient(value.nameInput, value.amountInput);
     this.shoppingListService.addIngredient(newIngredient);
-    
   }
 
 }
