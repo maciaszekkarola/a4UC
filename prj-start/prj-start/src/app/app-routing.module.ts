bdfea942e0f2d3fecd300ecb109c1b18e71604a9
Karolina <carolha.es@gmail.com>
2017-10-12 14:27:27 +0200

    lazy loading added

--- prj-start/prj-start/src/app/app-routing.module.ts
+++ prj-start/prj-start/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules } from '@angular/router';
 
 import { ShoppingListComponent } from 'app/components/shopping-list/shopping-list.component';
 
@@ -9,13 +10,11 @@ import { ShoppingListComponent } from 'app/components/shopping-list/shopping-lis
 const appRoutes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'recipes', loadChildren: './components/recipe-book/recipes.module#RecipesModule'},
-    // {path: 'shoppingList', loadChildren: './components/shopping-list/shopping.module#ShoppingModule'}
-    
     {path: 'shoppingList', component: ShoppingListComponent}
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(appRoutes)],
+    imports: [RouterModule.forRoot(appRoutes, {preloadingStrategy: PreloadAllModules})],
     exports: [RouterModule]
 })
 export class AppRoutingModule {
